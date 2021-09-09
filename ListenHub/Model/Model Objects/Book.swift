b10072beeff020dbbab6d486609840dec298683d
Burhan Aras <burhan.aras@paydustry.com>
2021-09-09 21:44:38 +0300

    Model objects for books and collections.

---
+++ ListenHub/Model/Model Objects/Book.swift
@@ -0,0 +1,25 @@
+//
+//  Book.swift
+//  ListenHub
+//
+//  Created by Burhan Aras on 9.09.2021.
+//
+
+import Foundation
+
+struct Collection: Identifiable {
+    let id: String
+    let books: [Book]
+}
+
+struct Book: Identifiable {
+    let id: String
+    let title: String
+    let author: Author
+    let image: URL
+}
+
+struct Author: Identifiable {
+    let id: String
+    let name: String
+}
---
+++ ListenHub/UI/Home/HomeView.swift
@@ -0,0 +1,26 @@
+//
+//  HomeView.swift
+//  ListenHub
+//
+//  Created by Burhan Aras on 9.09.2021.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+       CollectionsView()
+    }
+}
+
+struct CollectionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
